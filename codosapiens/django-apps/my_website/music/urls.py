ceafe76a7604f840389982c4818666315913d94a
AnuragAnalog <anurag.peddi1998@gmail.com>
2019-12-13 07:06:51 +0530

    backup31

--- codosapiens/django-apps/my_website/music/urls.py
+++ codosapiens/django-apps/my_website/music/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:album_id>', views.index1, name='index1'),
+    path('<int:album_id>/', views.index1, name='index1'),
     path('', views.index, name='index'),
 ]
--- codosapiens/django-apps/my_website/music/views.py
+++ codosapiens/django-apps/my_website/music/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 def index(request):
-    return HttpResponse("<h1> This is a music app </h1>")
+    html = ''
+    return HttpResponse(html)
+
+# def index(request):
+#     all_album = Album.objects.all()
+#     html = ''
+#     for album in all_album:
+#         url = '/music/'+str(album.id)+'/'
+#         html += '<a href="'+url+'"> '+album.artist+' </a><br>'
+#     return HttpResponse(html)
 
 def index1(request, album_id):
     return HttpResponse("<h2> This is View "+ str(album_id) +" Taylor </h2>")

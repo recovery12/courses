2c7e630be908d4369422388fcad5eeaddcda3d26
AnuragAnalog <anurag.peddi1998@gmail.com>
2019-12-13 17:48:40 +0530

    backup32

--- codosapiens/django-apps/my_website/music/views.py
+++ codosapiens/django-apps/my_website/music/views.py
@@ -1,10 +1,22 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
+#1 from django.template import loader
+from django.shortcuts import render # Using templates in a shortcut way
 from .models import Album
 
+# Write a template in a shortcut way
 def index(request):
-    html = ''
-    return HttpResponse(html)
+    all_albums = Album.objects.all()
+    context = {'all_albums':all_albums}
+    return render(request, 'music/index.html', context)
+
+#1 def index(request):
+#     all_albums = Album.objects.all()
+#     template = loader.get_template('music/index.html')
+#     html = ''
+#     context = {
+#         'all_albums':all_albums,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 # def index(request):
 #     all_album = Album.objects.all()
@@ -15,4 +27,8 @@ def index(request):
 #     return HttpResponse(html)
 
 def index1(request, album_id):
-    return HttpResponse("<h2> This is View "+ str(album_id) +" Taylor </h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album Does not Exists")
+    return render(request, 'music/index1.html', {'album':album})

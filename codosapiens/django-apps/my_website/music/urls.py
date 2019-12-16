0939d1f5ccd2c62ef67cfff956240425c021e116
AnuragAnalog <anurag.peddi1998@gmail.com>
2019-12-16 09:55:50 +0530

    backup34

--- codosapiens/django-apps/my_website/music/urls.py
+++ codosapiens/django-apps/my_website/music/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    path('<int:album_id>/', views.index1, name='index1'),
     path('', views.index, name='index'),
+    path('<int:album_id>/', views.index1, name='index1'),
+    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 ]
--- codosapiens/django-apps/my_website/music/views.py
+++ codosapiens/django-apps/my_website/music/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, Http404
 #1 from django.template import loader
-from django.shortcuts import render # Using templates in a shortcut way
-from .models import Album
+from django.shortcuts import render, get_object_or_404 # Using templates in a shortcut way
+from .models import Album, Song
 
 # Write a template in a shortcut way
 def index(request):
@@ -27,8 +27,23 @@ def index(request):
 #     return HttpResponse(html)
 
 def index1(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album Does not Exists")
+    # try:
+        # album = Album.objects.get(pk=album_id)
+    # except Album.DoesNotExist:
+        # raise Http404("Album Does not Exists")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/index1.html', {'album':album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/index1.html', {
+            'album': album,
+            'error_message': "You did not select a valid song"
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/index1.html', {'album':album})

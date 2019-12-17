62b40a83259d0d4aeada88af68d8811b6e568b4b
AnuragAnalog <anurag.peddi1998@gmail.com>
2019-12-17 18:52:53 +0530

    backup36

--- codosapiens/django-apps/my_website/my_website/settings.py
+++ codosapiens/django-apps/my_website/my_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_u2(&!p+hg9%x-@4%b948f-&mo%xcearx+$&*k5y_$6zn$kyq@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.5.0.145', 'localhost', '127.0.0.1']
 
 
 # Application definition
